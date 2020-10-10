c349e2b1489ddbc197dde35319532414a3ba8219
anupam-tiwari <anupam.tiwari@berkeley.edu>
2020-10-10 11:49:07 +0530

    first

---
+++ js/app-variables.js
@@ -0,0 +1,10 @@
+localStorage.setItem('UserPoolClientId', '7o3ubhjko2lptc8e785k4p51p7');localStorage.setItem('UserPoolId', 'us-west-2_mB5M9Tk64');localStorage.setItem('AnalyticsTable', 'Winko-AnalyticsTable-1TEG5QD78RRNK');localStorage.setItem('Region', 'us-west-2');localStorage.setItem('IdentityPoolId', 'us-west-2:aaebea2c-ee99-416f-9651-dfb4a8e23d4b');var _dashboard_usage = 'True'; 
+                var _hit_data = { 
+                    Solution: 'SO0039', 
+                    UUID: 'cb1307ad-536d-4ec1-85a7-15fc4bf376d5', 
+                    Data: { 
+                        dashboard: 1, 
+                        region: 'us-west-2' 
+                    } 
+                } 
+            
